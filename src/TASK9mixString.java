7c96cd1231fd87bd324fc7d0def7499b9466bc35
CompanyProfile <alanood94895@gmail.com>
2026-04-19 11:13:33 +0400

    TASK9mixString

--- src/TASK9mixString.java
+++ src/TASK9mixString.java
@@ -1,2 +1,26 @@
 public class TASK9mixString {
-}
+        public static void main(String[] args) {
+            System.out.println(TASK9mixString("abc", "xyz"));     // axbycz
+            System.out.println(TASK9mixString("Hi", "There"));    // HTihere
+            System.out.println(TASK9mixString("a", "pqrs"));      // apqrs
+        }
+
+        public static String TASK9mixString(String a, String b) {
+            String result = "";
+            int i = 0;
+
+            while (i < a.length() || i < b.length()) {
+
+                if (i < a.length()) {
+                    result += a.charAt(i);
+                }
+                if (i < b.length()) {
+                    result += b.charAt(i);
+                }
+
+                i++;
+            }
+            return result;
+        }
+    }
+
