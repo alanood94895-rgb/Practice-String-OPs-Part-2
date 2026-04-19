5dfda36745adaccf86c45c834a7140b24c6d80dc
CompanyProfile <alanood94895@gmail.com>
2026-04-19 14:17:12 +0400

    TASK21wordEnds

---
+++ src/TASK21wordEnds.java
@@ -0,0 +1,31 @@
+public class TASK21wordEnds {
+    public static void main(String[] args) {
+        System.out.println(wordEnds("abcXY123XYijk", "XY")); // c13i
+        System.out.println(wordEnds("XY123XY", "XY"));       // 13
+        System.out.println(wordEnds("XY1XY", "XY"));         // 11
+
+        // Zelda example
+        System.out.println(wordEnds("aZelda1Zeldab", "Zelda")); // a1b
+    }
+
+    public static String wordEnds(String str, String word) {
+        StringBuilder result = new StringBuilder();
+        int len = word.length();
+
+        for (int i = 0; i <= str.length() - len; i++) {
+
+            if (str.substring(i, i + len).equals(word)) {
+
+                if (i - 1 >= 0) {
+                    result.append(str.charAt(i - 1));
+                }
+
+                if (i + len < str.length()) {
+                    result.append(str.charAt(i + len));
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
