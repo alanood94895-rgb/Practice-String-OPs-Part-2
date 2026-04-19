b99a97d75603eb16d618970b8c5e50230821abf8
CompanyProfile <alanood94895@gmail.com>
2026-04-19 11:38:17 +0400

    TASK12repeatSeparator

---
+++ src/TASK12repeatSeparator.java
@@ -0,0 +1,22 @@
+public class TASK12repeatSeparator {
+    public static void main(String[] args) {
+
+        System.out.println(TASK12repeatSeparator("Word", "X", 3));   // WordXWordXWord
+        System.out.println(TASK12repeatSeparator("This", "And", 2));  // ThisAndThis
+        System.out.println(TASK12repeatSeparator("This", "And", 1));  //
+    }
+
+    public static String TASK12repeatSeparator(String word, String sep, int count) {
+        String result = "";
+
+        for (int i = 0; i < count; i++) {
+            result += word;
+
+            if (i < count - 1) {
+                result += sep;
+            }
+        }
+
+        return result;
+    }
+}
