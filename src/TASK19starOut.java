9417cfe03f78a3c7308931f593870844499fa3d2
CompanyProfile <alanood94895@gmail.com>
2026-04-19 12:37:58 +0400

    TASK19starOut

--- src/TASK19starOut.java
+++ src/TASK19starOut.java
@@ -2,7 +2,7 @@ public class TASK19starOut {
     public static void main(String[] args) {
         System.out.println(starOut("ab*cd"));     //ad
         System.out.println(starOut("ab**cd"));    //ad
-        System.out.println(starOut("sm*eilly"));  //
+        System.out.println(starOut("sm*eilly"));  //silly
 
 
     }
