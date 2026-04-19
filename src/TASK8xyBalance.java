2dcfa7eaa74b6f285a8ea1c7f21c80c5ed89930d
CompanyProfile <alanood94895@gmail.com>
2026-04-19 11:06:04 +0400

    TASK8xyBalance

---
+++ src/TASK8xyBalance.java
@@ -0,0 +1,25 @@
+public class TASK8xyBalance {
+    public static void main (String[] args){
+        System.out.println(TASK8xyBalance ("aaxbby"));
+        System.out.println(TASK8xyBalance ("aaxbb"));
+        System.out.println(TASK8xyBalance ("yaaxbb"));
+
+    }
+    public static boolean TASK8xyBalance(String str) {
+        boolean Y = false;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char c = str.charAt(i);
+
+            if (c == 'y') {
+                Y = true;
+            }
+
+            if (c == 'x' && !Y) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
