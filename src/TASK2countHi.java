b2f825359c13828091ddc718c20b312edc8e1b20
CompanyProfile <alanood94895@gmail.com>
2026-04-19 14:24:25 +0400

    TASK2countHi

--- src/TASK2countHi.java
+++ src/TASK2countHi.java
@@ -1,6 +1,6 @@
 public class TASK2countHi {
     public static void main(String[] args) {
-        System.out.println(TASK2countHi ("abc hi ho")); //
+        System.out.println(TASK2countHi ("abc hi ho")); //1
         System.out.println(TASK2countHi("ABChi hi"));  //
         System.out.println(TASK2countHi("hihi"));      //
     }
