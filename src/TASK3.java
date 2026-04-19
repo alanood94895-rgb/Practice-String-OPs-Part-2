ae74957e105aba40de9e517836b7d1a83a297f3b
CompanyProfile <alanood94895@gmail.com>
2026-04-19 10:34:26 +0400

    TASk3

---
+++ src/TASK3.java
@@ -0,0 +1,27 @@
+public class TASK3 {
+    public static void main (String[] args){
+        System.out.println( TASK3("catdog"));
+        System.out.println(TASK3 ("catcat"));
+        System.out.println(TASK3 ("1cat1cadodog"));
+
+    }
+    public static boolean TASK3(String str) {
+        int catCount = 0;
+        int dogCount = 0;
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            String sub = str.substring(i, i + 3);
+
+            if (sub.equals("cat")) {
+                catCount++;
+            }
+
+            if (sub.equals("dog")) {
+                dogCount++;
+            }
+        }
+
+        return catCount == dogCount;
+    }
+
+}
