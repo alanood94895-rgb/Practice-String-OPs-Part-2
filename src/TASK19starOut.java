5130842c551a00809b7318790279d19940ae5c24
CompanyProfile <alanood94895@gmail.com>
2026-04-19 12:36:41 +0400

    TASK19starOut

---
+++ src/TASK19starOut.java
@@ -0,0 +1,33 @@
+public class TASK19starOut {
+    public static void main(String[] args) {
+        System.out.println(starOut("ab*cd"));     // ad
+        System.out.println(starOut("ab**cd"));    // ad
+        System.out.println(starOut("sm*eilly"));  // silly
+
+        // Zelda example
+        System.out.println(starOut("Ze*lda"));    // Zda
+    }
+
+    public static String starOut(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == '*') {
+                continue;
+            }
+
+            if (i > 0 && str.charAt(i - 1) == '*') {
+                continue;
+            }
+
+            if (i < str.length() - 1 && str.charAt(i + 1) == '*') {
+                continue;
+            }
+
+            result.append(str.charAt(i));
+        }
+
+        return result.toString();
+    }
+}
