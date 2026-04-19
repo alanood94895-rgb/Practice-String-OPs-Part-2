1d0a0a7762f1888ac6d53413c83fadc4de73982d
CompanyProfile <alanood94895@gmail.com>
2026-04-19 14:28:33 +0400

    TASK4countCode

--- src/TASK4countCode.java
+++ src/TASK4countCode.java
@@ -1,6 +1,6 @@
 public class TASK4countCode {
         public static void main (String[] args){
-            System.out.println(TASK4countCode ("aaacodebbb")); // 1
+            System.out.println(TASK4countCode ("aaacodebbb")); //
             System.out.println(TASK4countCode ("codexxcode"));  //
             System.out.println(TASK4countCode ("cozexxcope"));//
 
