346076f0526dde43759648c4cec094e7b2903da6
CompanyProfile <alanood94895@gmail.com>
2026-04-19 12:30:55 +0400

    TASK18zipZap

--- src/TASK18zipZap.java
+++ src/TASK18zipZap.java
@@ -10,7 +10,6 @@ public class TASK18zipZap {
 
             for (int i = 0; i < str.length(); i++) {
 
-                // Check for "z?p" pattern
                 if (i < str.length() - 2 &&
                         str.charAt(i) == 'z' &&
                         str.charAt(i + 2) == 'p') {
