d530b50f81aedb43c6f7eae34712c5bbdec451f7
CompanyProfile <alanood94895@gmail.com>
2026-04-19 11:31:00 +0400

    TASK11repeatFront

---
+++ src/TASK11repeatFront.java
@@ -0,0 +1,18 @@
+public class TASK11repeatFront {
+    public static void main(String[] args) {
+        System.out.println(TASK11repeatFront("Chocolate", 4)); // ChocChoChC
+        System.out.println(TASK11repeatFront("Chocolate", 3));
+        System.out.println(TASK11repeatFront("Ice Cream", 2));
+    }
+
+    public static String TASK11repeatFront(String str, int n) {
+        String result = "";
+
+        for (int i = n; i > 0; i--) {
+            result += str.substring(0, i);
+        }
+
+        return result;
+    }
+}
+
