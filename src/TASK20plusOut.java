c661b70aab91a310e8f098f74a84f977cd442042
CompanyProfile <alanood94895@gmail.com>
2026-04-19 14:12:57 +0400

    TASK20plusOut

---
+++ src/TASK20plusOut.java
@@ -0,0 +1,30 @@
+public class TASK20plusOut {
+    public static void main(String[] args) {
+        System.out.println(plusOut("12xy34", "xy"));         // ++xy++
+        System.out.println(plusOut("12xy34", "1"));          // 1+++++
+        System.out.println(plusOut("12xy34xyabcxy", "xy"));  // ++xy++xy+++xy
+
+        // Zelda example
+        System.out.println(plusOut("Ze1ldaZelda123", "Zelda")); // +++++Zelda+++++
+    }
+
+    public static String plusOut(String str, String word) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); ) {
+
+            if (i <= str.length() - word.length() &&
+                    str.substring(i, i + word.length()).equals(word)) {
+
+                result.append(word);
+                i += word.length();
+
+            } else {
+                result.append('+');
+                i++;
+            }
+        }
+
+        return result.toString();
+    }
+}
