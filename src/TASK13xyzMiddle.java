a2cb5535e113e7c6181c6027ff6c7b9840161d73
CompanyProfile <alanood94895@gmail.com>
2026-04-19 11:48:50 +0400

    TASK13xyzMiddle

--- src/TASK13xyzMiddle.java
+++ src/TASK13xyzMiddle.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class TASK13xyzMiddle {
+    public static void main(String[] args) {
+        System.out.println(TASK13xyzMiddle("abXYabc", 1)); // TRUE
+        System.out.println(TASK13xyzMiddle("abXYabc", 2)); // TRUE
+        System.out.println(TASK13xyzMiddle("abXYabc", 3)); // FALSE
+    }
+
+    public static boolean TASK13xyzMiddle(String str, int n) {
+        String prefix = str.substring(0, n);
+
+        for (int i = n; i <= str.length() - n; i++) {
+            if (str.substring(i, i + n).equals(prefix)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
