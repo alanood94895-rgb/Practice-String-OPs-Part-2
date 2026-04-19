450c123c6855df3c2a95e2d26067ec0e962cb27c
CompanyProfile <alanood94895@gmail.com>
2026-04-19 11:54:22 +0400

    TASK14xyzMiddle

--- src/TASK14xyzMiddle.java
+++ src/TASK14xyzMiddle.java
@@ -1,2 +1,26 @@
 public class TASK14xyzMiddle {
+    public static void main(String[] args) {
+        System.out.println(TASK14xyzMiddle("AAxyzBB"));   // true
+        System.out.println(TASK14xyzMiddle("AxyzBB"));    // true
+        System.out.println(TASK14xyzMiddle("AxyzBBB"));   // false
+    }
+
+    public static boolean TASK14xyzMiddle(String str) {
+        int n = str.length();
+
+        for (int i = 0; i <= n - 3; i++) {
+            if (str.substring(i, i + 3).equals("xyz")) {
+
+                int left = i;
+                int right = n - (i + 3);
+
+                if (Math.abs(left - right) <= 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
