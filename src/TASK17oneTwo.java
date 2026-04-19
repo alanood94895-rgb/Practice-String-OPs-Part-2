323b43c22cceb7aecccb50f4266bcd14f70c9df0
CompanyProfile <alanood94895@gmail.com>
2026-04-19 12:25:10 +0400

    TASK17oneTwo

--- src/TASK17oneTwo.java
+++ src/TASK17oneTwo.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class TASK17oneTwo {
+    public static void main(String[] args) {
+        System.out.println(oneTwo("abc"));       // bca
+        System.out.println(oneTwo("tca"));       // cat
+        System.out.println(oneTwo("tcagdo"));    // catdog
+    }
+
+    public static String oneTwo(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length() - 2; i += 3) {
+            String group = str.substring(i, i + 3);
+
+            // move first character to the end
+            result += group.substring(1) + group.charAt(0);
+        }
+
+        return result;
+    }
+
 }
