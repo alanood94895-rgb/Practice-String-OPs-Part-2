12497f878b58e33db802e1506f1823c31fc4e566
CompanyProfile <alanood94895@gmail.com>
2026-04-19 15:27:22 +0400

    TASK14xyzMiddle

--- src/TASK14xyzMiddle.java
+++ src/TASK14xyzMiddle.java
@@ -1,7 +1,7 @@
 public class TASK14xyzMiddle {
     public static void main(String[] args) {
         System.out.println(TASK14xyzMiddle("AAxyzBB"));   // True
-        System.out.println(TASK14xyzMiddle("AxyzBB"));    //
+        System.out.println(TASK14xyzMiddle("AxyzBB"));    // True
         System.out.println(TASK14xyzMiddle("AxyzBBB"));   //
     }
 
