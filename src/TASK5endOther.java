c7f859d9e2093a43d75a0d425c4f9638e21be2a7
CompanyProfile <alanood94895@gmail.com>
2026-04-19 11:09:34 +0400

    TASK5endOther

--- src/TASK5endOther.java
+++ src/TASK5endOther.java
@@ -1,8 +1,8 @@
 public class TASK5endOther {
     public static void main(String[] args) {
-        System.out.println(TASK5endOther("Hiabc", "abc"));     // true
-        System.out.println(TASK5endOther("AbC", "HiaBc"));     // true
-        System.out.println(TASK5endOther("abc", "abXabc"));    // true
+        System.out.println(TASK5endOther("Hiabc", "abc"));     // TRUE
+        System.out.println(TASK5endOther("AbC", "HiaBc"));     // TRUE
+        System.out.println(TASK5endOther("abc", "abXabc"));    // TRUE
     }
 
     public static boolean TASK5endOther(String a, String b) {
