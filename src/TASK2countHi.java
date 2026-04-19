c21afb85a8fa251528a80e7c196e31e3c8d2145f
CompanyProfile <alanood94895@gmail.com>
2026-04-19 14:23:46 +0400

    TASK2countHi

--- src/TASK2countHi.java
+++ src/TASK2countHi.java
@@ -1,11 +1,11 @@
-public class TASK2 {
+public class TASK2countHi {
     public static void main(String[] args) {
-        System.out.println(TASK2 ("abc hi ho")); // 1
-        System.out.println(TASK2("ABChi hi"));  // 2
-        System.out.println(TASK2("hihi"));      // 2
+        System.out.println(TASK2countHi ("abc hi ho")); // 1
+        System.out.println(TASK2countHi("ABChi hi"));  // 2
+        System.out.println(TASK2countHi("hihi"));      // 2
     }
 
-    public static int TASK2(String str) {
+    public static int TASK2countHi(String str) {
         int count = 0;
 
         for (int i = 0; i < str.length() - 1; i++) {
